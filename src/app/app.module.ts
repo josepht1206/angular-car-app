b2d351810c915416b9d800781b0b2dd3b52be393
joseph.tan@hexinitiative.com <joseph.tan@hexinitiative.com>
2023-05-16 16:01:14 +0800

    [] Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgetPassComponent } from './pages/forget-pass/forget-pass.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ChangePassComponent } from './pages/change-pass/change-pass.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ForgetPassComponent,
+    ProfileComponent,
+    ChangePassComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    SideNavComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
