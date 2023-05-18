8979c9256f9097fee453b6e842dc9410124543f0
Joseph.T <joseph.tan@hexainitiative.com>
2023-05-18 19:19:17 +0800

    [] Implement cdr for logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.getIsLoggedIn();
+    console.log(this.isLoggedIn);
+  }
 
   onLogout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.cdr.detectChanges();
   }
 }
