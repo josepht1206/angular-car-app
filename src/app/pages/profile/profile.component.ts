c76ae70c2578495778fe511f48b571a90d1793c1
Joseph.T <joseph.tan@hexainitiative.com>
2023-05-19 16:07:54 +0800

    [] Disply username and email on profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  loggedInUser: string | null | undefined;
+  username = '';
+  email = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      this.loggedInUser = user;
+      this.username = this.getUsernameWithoutDomain(user);
+    });
+  }
+
+  getUsernameWithoutDomain(email: string | null): string {
+    if (email) {
+      const atIndex = email.indexOf('@');
+      if (atIndex !== -1) {
+        return email.substring(0, atIndex);
+      }
+    }
+    return '';
+  }
 }
