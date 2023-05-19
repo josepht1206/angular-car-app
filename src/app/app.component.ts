785908e81e2ee8078901386cd634d36573b68de3
Joseph.T <joseph.tan@hexainitiative.com>
2023-05-19 11:38:18 +0800

    [] show sidenav only if login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor() {}
+  showSideNav = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check the current route and set showSideNav accordingly
+        this.showSideNav = this.shouldShowSideNav(event.url);
+      }
+    });
+  }
+
+  private shouldShowSideNav(url: string): boolean {
+    // Define the routes where the side navigation should be displayed
+    const allowedRoutes = [
+      '/dashboard',
+      '/profile',
+      '/change-pass',
+      '/products',
+    ];
+
+    // Check if the current route is in the allowed routes list
+    return allowedRoutes.includes(url);
+  }
 }
